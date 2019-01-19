24e9e6a1e7bb754dc7d799ba1dfb4b42eececa91
Hau Trung Nguyen <haunt.hcm2015@gmail.com>
2019-01-19 13:36:34 +0700

    Create client_side.sh

---
+++ client_side.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PATH=${PATH}:/sbin:/usr/sbin:/bin:/usr/bin:/usr/pkg/bin
+
+name="mybox"
+domain="mydomain"
+fqdn="${name}.${domain}"
+# access into Ubuntu server 
+auth="user:password"
+
+# retrieve your actual public IP from a website like httpbin.org
+curip=$(curl -s -o- http://httpbin.org/ip)
+# fetch recorded IP address
+homeip=$(curl -u ${auth} -s -o- https://your.public.server/dyndns/query/a/${fqdn})
+
+if [ "${curip}" != "${homeip}" ]; then
+        warnmsg="/!\\ home IP changed to ${curip} /!\\"
+
+        echo "${warnmsg}"|mail -s "${warnmsg}" me@mydomain.net
+
+        curl -u ${auth} \
+                -X POST https://my.public.server/dyndns/update/${domain}/${name}/${curip}
+fi
